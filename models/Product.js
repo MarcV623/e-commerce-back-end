83ae803bc1c40fd380556929017b4b3774683995
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-08-28 14:46:45 -0500

    added fields to product table

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,28 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    productName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    price: {
+      allowNull: false,
+      isDecimal: true,
+      type: DataTypes.DECIMAL
+    },
+    stock: {
+      allowNull: false,
+      isInteger: true,
+      type: DataTypes.INTEGER,
+      defaultValue: 10
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
